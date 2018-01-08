13853a3f1f60957f79a88d33b5dac6c99179eaef
Arvis Abromovics <info@vsmedio.lv>
2018-01-08 16:53:34 +0100

    Initial commit

---
+++ src/Solution.java
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+/*
+Работа с датой
+1. Реализовать метод isDateOdd(String date) так, чтобы он возвращал true, если количество дней с начала года -
+нечетное число, иначе false
+2. String date передается в формате MAY 1 2013
+Не забудьте учесть первый день года.
+
+Пример:
+JANUARY 1 2000 = true
+JANUARY 2 2020 = false
+
+
+Требования:
+1. Программа должна выводить текст на экран.
+2. Класс Solution должен содержать два метода.
+3. Метод isDateOdd() должен возвращать true, если количество дней с начала года - нечетное число, иначе false.
+4. Метод main() должен вызывать метод isDateOdd().
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+//        System.out.println(isDateOdd("JANUARY 1 2000"));
+//        System.out.println(isDateOdd("JANUARY 2 2020"));
+        System.out.println(isDateOdd("MAY 1 2013"));
+    }
+
+    public static boolean isDateOdd(String date) {
+        Date dt1 = new Date(date);
+        Date dt2 = new Date(date);
+        dt1.setMonth(0);
+        dt1.setDate(1);
+
+//        System.out.println(dt1.getTime());
+//        System.out.println(dt2.getTime());
+        long a = (dt2.getTime()-dt1.getTime())/(3600*24*1000)+1;
+        if (a % 2 != 0)
+            return true;
+        //System.out.println(dt2.getTime()-dt1.getTime());
+        //System.out.println (a);
+        return false;
+    }
+}
